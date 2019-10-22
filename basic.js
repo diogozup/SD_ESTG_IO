5ffd1f69b7eac2e6982fc274654e7c11abf64ef9
PandaZup <diogomagalhaes17@hotmail.com>
2019-10-22 16:19:01 +0100

    2nd commit

--- basic.js
+++ basic.js
@@ -3,14 +3,23 @@ const http = require('http')
 // we need socketio.. its a 3rd party
 const socketio = require('socket.io');
 
-
-
-// make an htttp server with node!
+//? make an htttp server with node!
 const server = http.createServer((req,res)=> {
     res.end("I am connected")
 });
 
+
 const io = socketio(server);
 
+io.on('connection',(socket,req)=>{
+    socket.emit('Welcome to the websocket server!!')
+    socket.on('message',(msg)=>{
+        console.log(msg)
+    })
+})
 
+
+
+
+// server
 server.listen(8000);
